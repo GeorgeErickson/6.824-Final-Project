e46d5b5ff6edf715803fed65369652d8777fed4e
David Benhaim <davidbenhaim12@gmail.com>
2013-05-14 10:37:50 -0400

    letting people see their own edits

--- src/server/hub/hub.go
+++ src/server/hub/hub.go
@@ -139,7 +139,6 @@ func (h *DocumentHub) Run() {
             	fmt.Println("Error:", err, string(m))
             	continue
         	}
-        	fmt.Println(h.Document.Snapshot, doc)
         	error := h.Document.ApplyOps(doc.OpData[0], doc.Version)
         	if !error {
         		json_bytes, _ := json.Marshal(h.Document)
@@ -152,9 +151,6 @@ func (h *DocumentHub) Run() {
 		    	h.Save(redis_conn)
 		    }
 			for c := range h.Connections {
-				if c == conn{
-					continue
-				}
 				select {
 				case c.Send <- Message{M:json_bytes}:
 				default:
